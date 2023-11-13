704d1dace116d382f6bdb93932750b16694427ac
dotollll <k44790488@gmail.com>
2023-11-13 16:43:48 +0900

    section2 finish

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,25 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+
+    @GetMapping("/hi") // URL 연결 요청
+    public String niceToMeetYou(Model model) {
+        // model 객체가 "홍팍" 값을 "username"에 연결해 웹 브라우저로 보냄
+        model.addAttribute("username", "hongpark");
+
+        // greetings.mustache 파일을 찾아 웹 브라우저로 전송
+        return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname", "홍길동");
+        return "goodbye";
+    }
+}
